5f0833f834c3bb2f41cf778227e3b7b6d2ea5571
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-03 00:07:26 +0530

    ADD: Welcome message

---
+++ main/java/com/userRegistrationSystem/UserRegistrationSystem.java
@@ -0,0 +1,9 @@
+package com.userRegistrationSystem;
+
+public class UserRegistrationSystem
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to User Registration System!");
+    }
+}
---
+++ test/java/com/userRegistrationSystem/UserRegistrationSystem.java
@@ -0,0 +1,13 @@
+package com.userRegistrationSystem;
+
+import org.junit.Test;
+
+public class UserRegistrationSystemTest
+{
+    @Test
+    public void name()
+    {
+        UserRegistrationSystem userRegistrationSystem = new UserRegistrationSystem();
+
+    }
+}
